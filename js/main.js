2810f2ee3fa8c0e01e67dcaa02b01e6fb5303f87
Zoran Smiljanic <zoran.smilljanic@gmail.com>
2019-02-21 13:28:00 +0100

    inicijalizacija

---
+++ js/main.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+    
+    
+    
+    //FILL ELEMENTS DEPENDS OF CATEGORY
+    $('[data-category]').each(function () {
+        var color = $(this).data('category');
+        $(this).find('.category-bg').css('background', color);
+        $(this).find('.category-border').css('border-color', color);
+        $(this).find('.category-color').css('color', color );
+    });
+    
+    
+    // EASE SCROLL
+
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 800);
+    });
+    
+    
+    //ANIMATION 
+    
+    function animation() {
+        var windowHight = $(window).height();
+        var scroll = $(window).scrollTop();
+        $('.animation').each(function () {
+            var position = $(this).offset().top;
+            var animation = $(this).attr('data-animation');
+            //var delay = $(this).attr('data-delay');
+            if (position < scroll + windowHight - 60) {
+                $(this).addClass(animation);
+                //$(this).css('animation-delay', delay);
+            }
+            console.log(position);
+        });
+
+    }
+
+    animation();
+
+    $(window).scroll(function () {
+        animation();
+    });
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+});//end document.ready
+
+
